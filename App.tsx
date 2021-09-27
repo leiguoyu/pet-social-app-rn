e1295a9f37e832cfd485b645d9ccdcdcb47d39ef
andy.lei <andy.lei@skysoft.com>
2021-09-27 10:06:21 +0800

    1. 新增根路径别名配置，使用@

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './js/redux/store';
 import AppNavigator from './js/navigation/AppNavigator';
+import {api} from '@/api';
+debugger;
+api.user.getUserInfo((data: any) => {
+  console.log(data);
+});
+const App = () => (
+  <Provider store={store}>
+    <AppNavigator />
+  </Provider>
+);
 
-
-
-const App = () => {
-  return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
-  );
-}
-
-
-
-export default App; 
+export default App;
