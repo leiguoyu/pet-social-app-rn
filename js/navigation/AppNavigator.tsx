3dc40acf56054d79d4be920ce6bf474fbb579570
niki.zhu <niki.zhu@cdskysoft.com>
2022-01-05 17:45:26 +0800

    APP介绍页面开发100%

--- js/navigation/AppNavigator.tsx
+++ js/navigation/AppNavigator.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {Component, useState} from 'react';
+import {AsyncStorage, Alert} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -27,7 +28,7 @@ Assets.loadAssetsGroup('icons', {
 });
 
 const Stack = createNativeStackNavigator();
-const no_token_page_JSX = (
+const first_no_token_page_JSX = (
   <>
     <Stack.Screen
       name="AppIntro"
@@ -36,6 +37,105 @@ const no_token_page_JSX = (
         headerShown: false,
       }}
     />
+    <Stack.Screen
+      name="SignIn"
+      component={SignIn}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <Stack.Screen
+      name="Login"
+      component={Login}
+      options={{
+        title: '',
+        headerShown: true, // 显示header
+        headerTransparent: true, // 设置header绝对定位且背景透明
+      }}
+    />
+    <Stack.Screen
+      name="SignUpStep1"
+      component={SignUpStep1}
+      options={{
+        title: '',
+        headerShown: true, // 显示header
+        headerTransparent: true, // 设置header绝对定位且背景透明
+      }}
+    />
+    <Stack.Screen
+      name="SignUpStep2"
+      component={SignUpStep2}
+      options={{
+        title: '',
+        headerShown: true, // 显示header
+        headerTransparent: true, // 设置header绝对定位且背景透明
+      }}
+    />
+    <Stack.Screen
+      name="SignUpComplete"
+      component={SignUpComplete}
+      options={{
+        headerShown: false, // 隐藏header
+      }}
+    />
+    <Stack.Screen
+      name="AddNewPetStep1"
+      component={AddNewPetStep1}
+      options={{
+        title: '',
+        headerShown: true, // 显示header
+        headerTransparent: true, // 设置header绝对定位且背景透明
+      }}
+    />
+    <Stack.Screen
+      name="AddNewPetStep2"
+      component={AddNewPetStep2}
+      options={{
+        title: '',
+        headerShown: true, // 显示header
+        headerTransparent: true, // 设置header绝对定位且背景透明
+      }}
+    />
+    <Stack.Screen
+      name="AddNewPetStep3"
+      component={AddNewPetStep3}
+      options={{
+        title: '',
+        headerShown: true, // 显示header
+        headerTransparent: true, // 设置header绝对定位且背景透明
+      }}
+    />
+    <Stack.Screen
+      name="AddNewPetStep4"
+      component={AddNewPetStep4}
+      options={{
+        title: '',
+        headerShown: true, // 显示header
+        headerTransparent: true, // 设置header绝对定位且背景透明
+      }}
+    />
+    <Stack.Screen
+      name="AddNewPetStep5"
+      component={AddNewPetStep5}
+      options={{
+        title: '',
+        headerShown: true, // 显示header
+        headerTransparent: true, // 设置header绝对定位且背景透明
+      }}
+    />
+    <Stack.Screen
+      name="AddNewPetStep6"
+      component={AddNewPetStep6}
+      options={{
+        title: '',
+        headerShown: true, // 显示header
+        headerTransparent: true, // 设置header绝对定位且背景透明
+      }}
+    />
+  </>
+);
+const no_token_page_JSX = (
+  <>
     <Stack.Screen
       name="SignIn"
       component={SignIn}
@@ -160,30 +260,80 @@ const token_page_JSX = (
   </>
 );
 
-const AppNavigator = () => {
-  let [user_token, TokenChange] = useState('');
-  // 每天store数据发生变化，执行该钩子
-  store.subscribe(() => {
-    // TODO 每次state改变都会进来，待优化
-    // 获取state对象
-    let state: any = store.getState();
-    // 更新token no_token_page_JSX
-    TokenChange(state.user.token);
-  });
+// const AppNavigator = () => {
+//   let [user_token, TokenChange] = useState('');
+//   // 每天store数据发生变化，执行该钩子
+//   store.subscribe(() => {
+//     // TODO 每次state改变都会进来，待优化
+//     // 获取state对象
+//     let state: any = store.getState();
+//     // 更新token no_token_page_JSX
+//     TokenChange(state.user.token);
+//   });
 
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={!user_token ? 'AppIntro' : 'Home'}
-          screenOptions={{
-            headerShadowVisible: false,
-          }}>
-          {!user_token ? no_token_page_JSX : token_page_JSX}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
-};
+//   return (
+//     <SafeAreaProvider>
+//       <NavigationContainer>
+//         <Stack.Navigator
+//           initialRouteName={!user_token ? 'AppIntro' : 'Home'}
+//           screenOptions={{
+//             headerShadowVisible: false,
+//           }}>
+//           {!user_token ? no_token_page_JSX : token_page_JSX}
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </SafeAreaProvider>
+//   );
+// };
+
+class AppNavigator extends Component {
+  state = {
+    user_token: '',
+    noTokenPageName: 'AppIntro',
+  };
+  componentWillMount() {
+    this.openApp();
+  }
+  componentDidMount() {
+    store.subscribe(() => {
+      // TODO 每次state改变都会进来，待优化
+      // 获取state对象
+      let state: any = store.getState();
+      // 更新token no_token_page_JSX
+      this.setState({user_token: state.user.token});
+    });
+  }
+  openApp = () => {
+    AsyncStorage.getItem('isFirstLaunch', (error, result) => {
+      if (result === 'false') {
+        this.setState({noTokenPageName: 'SignIn'});
+      } else {
+        // 存储
+        AsyncStorage.setItem('isFirstLaunch', 'false');
+      }
+    });
+  };
+  render() {
+    return (
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName={
+              !this.state.user_token ? this.state.noTokenPageName : 'Home'
+            }
+            screenOptions={{
+              headerShadowVisible: false,
+            }}>
+            {!this.state.user_token
+              ? this.state.noTokenPageName === 'AppIntro'
+                ? first_no_token_page_JSX
+                : no_token_page_JSX
+              : token_page_JSX}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    );
+  }
+}
 
 export default AppNavigator;
--- js/pages/appIntro/AppIntro.tsx
+++ js/pages/appIntro/AppIntro.tsx
@@ -1,62 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StatusBar, Dimensions, StyleSheet} from 'react-native';
 import Swiper from 'react-native-swiper';
 import {View, Image, Text, Button} from 'react-native-ui-lib';
 
 const {width, height} = Dimensions.get('window'); // 获取手机的宽和高
 
-const AppIntro = () => (
-  <View style={styles.container}>
-    <StatusBar
-      backgroundColor="transparent"
-      translucent={true}
-      barStyle="light-content"
-    />
-    <Swiper
-      dotStyle={styles.dot}
-      activeDotColor="#fa4169"
-      loop={false}
-      index={0}>
-      <View style={styles.slide1}>
-        <Image style={styles.image} source={require('./img/BG_1.png')} />
-        <View style={styles.info}>
-          <Text style={styles.title}>宠物社交应用</Text>
-          <Text style={styles.description}>
-            我们把来自全国各地的宠物主人和动物爱好者聚在一起！
-          </Text>
+const AppIntro = ({navigation}) => {
+  const [index, onChangeIndex] = React.useState(0);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent={true}
+        barStyle="light-content"
+      />
+      <Swiper
+        dotStyle={styles.dot}
+        activeDotColor="#fa4169"
+        loop={false}
+        autoplay={true}
+        autoplayTimeout={5}
+        index={index}
+        onIndexChanged={index => {
+          onChangeIndex(index);
+        }}>
+        <View style={styles.slide1}>
+          <Image style={styles.image} source={require('./img/BG_1.png')} />
+          <View style={styles.info}>
+            <Text style={styles.title}>宠物社交应用</Text>
+            <Text style={styles.description}>
+              我们把来自全国各地的宠物主人和动物爱好者聚在一起！
+            </Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.slide2}>
-        <Image style={styles.image} source={require('./img/BG_2.png')} />
-        <View style={styles.info}>
-          <Text style={styles.title}>展示你的宠物日常生活</Text>
-          <Text style={styles.description}>
-            创建你的宠物个人账户，分享日常的生活动态
-          </Text>
+        <View style={styles.slide2}>
+          <Image style={styles.image} source={require('./img/BG_2.png')} />
+          <View style={styles.info}>
+            <Text style={styles.title}>展示你的宠物日常生活</Text>
+            <Text style={styles.description}>
+              创建你的宠物个人账户，分享日常的生活动态
+            </Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.slide3}>
-        <Image style={styles.image} source={require('./img/BG_3.png')} />
-        <View style={styles.info}>
-          <Text style={styles.title}>活动行为跟踪</Text>
-          <Text style={styles.description}>
-            通过应用功能你可以全天24小时查看跟踪你的宠物日常行为
-          </Text>
+        <View style={styles.slide3}>
+          <Image style={styles.image} source={require('./img/BG_3.png')} />
+          <View style={styles.info}>
+            <Text style={styles.title}>活动行为跟踪</Text>
+            <Text style={styles.description}>
+              通过应用功能你可以全天24小时查看跟踪你的宠物日常行为
+            </Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.slide4}>
-        <Image style={styles.image} source={require('./img/BG_4.png')} />
-        <View style={styles.info}>
-          <Text style={styles.title}>更健康，更快乐的宠物</Text>
-          <Text style={styles.description}>
-            在健康记录之外给你心爱的宠物最好的照顾
-          </Text>
+        <View style={styles.slide4}>
+          <Image style={styles.image} source={require('./img/BG_4.png')} />
+          <View style={styles.info}>
+            <Text style={styles.title}>更健康，更快乐的宠物</Text>
+            <Text style={styles.description}>
+              在健康记录之外给你心爱的宠物最好的照顾
+            </Text>
+          </View>
+          <Button
+            label="马上进入"
+            style={styles.continueBtn}
+            onPress={() => {
+              navigation.navigate('SignIn');
+            }}
+          />
         </View>
-        <Button label="马上进入" style={styles.continueBtn} />
-      </View>
-    </Swiper>
-  </View>
-);
+      </Swiper>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -92,11 +107,13 @@ const styles = StyleSheet.create({
   dot: {
     borderWidth: 1,
     borderColor: '#69707f',
+    backgroundColor: 'transparent',
   },
   info: {
     position: 'absolute',
     left: 0,
     right: 0,
+    bottom: 152,
     padding: 32,
   },
   title: {
